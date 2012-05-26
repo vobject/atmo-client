22db061ff6cc2ce4b101226f3ff33ba34b76162d
vobject <vobject@gmail.com>
2012-05-26 06:29:02 +0200

    further work on atmo-dummy script

--- arduino/atmo-dummy.py
+++ arduino/atmo-dummy.py
@@ -3,6 +3,17 @@ Test script to check the output of the VLC atmolight plugin.
 
 Connect to a (virtual) COM port where the atmolight protocol is send
 through and display the content.
+
+1. Create a virtual COM port using socat:
+    $> socat PTY,link=ttyS10 PTY,link=ttyS11
+
+2. Setup VLC by enabling the AtmoLight Filter:
+    - Check Preferences -> Video -> Filters -> AtmoLight Filter
+    - Enter the virtual serial port into the AtmoLight Filter's property window
+    - Save
+    - Restart VLC and start a video (check console output for errors)
+
+3. Start this script (Python 3)
 """
 
 import tkinter as tk
@@ -13,7 +24,7 @@ import time
 import threading
 import traceback
 
-channel_left = [0, 0, 0]
+protocol = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 class SerialThread(threading.Thread):
     def __init__(self, com_port):
@@ -24,12 +35,8 @@ class SerialThread(threading.Thread):
         try:
             self.open_serial()
             while True:
-                #data = self.connection.read(19)
-                #data_ord = [ord(x) for x in data]
-                data_ord = self.connection.read(19)
-                channel_left[0] = data_ord[7]
-                channel_left[1] = data_ord[8]
-                channel_left[2] = data_ord[9]
+                for index, item in enumerate(self.connection.read(19)):
+                    protocol[index] = item
         except KeyboardInterrupt:
             self.close_serial()
 
@@ -43,32 +50,44 @@ class SerialThread(threading.Thread):
     def close_serial(self):
         self.connection.close()
 
+class ChannelWidget(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+
+        self.r = tk.IntVar()
+        self.g = tk.IntVar()
+        self.b = tk.IntVar()
+
+        self.prg_channel_r = ttk.Progressbar(self, orient=tk.VERTICAL, length=255, mode="determinate", maximum=255, variable=self.r)
+        self.prg_channel_g = ttk.Progressbar(self, orient=tk.VERTICAL, length=255, mode="determinate", maximum=255, variable=self.g)
+        self.prg_channel_b = ttk.Progressbar(self, orient=tk.VERTICAL, length=255, mode="determinate", maximum=255, variable=self.b)
+
+        self.grid(column=3, row=1)
+        self.prg_channel_r.grid(column=0, row=0)
+        self.prg_channel_g.grid(column=1, row=0)
+        self.prg_channel_b.grid(column=2, row=0)
+
+    def update(self, r, g, b):
+        self.r.set(r)
+        self.g.set(g)
+        self.b.set(b)
+
 class AtmoDummy(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        self.content = ttk.Frame(self)
 
-        self.left_r = tk.IntVar()
-        self.left_g = tk.IntVar()
-        self.left_b = tk.IntVar()
+        self.mainframe = ttk.Frame(self)
+        self.mainframe.grid(column=4, row=1)
 
-        #self.progress_channel0 = ttk.Progressbar(self, orient=tk.VERTICAL, length=255, mode="determinate", maximum=255)
-        self.progress_channel1_r = ttk.Progressbar(self, orient=tk.VERTICAL, length=255, mode="determinate", maximum=255, variable=self.left_r)
-        self.progress_channel1_g = ttk.Progressbar(self, orient=tk.VERTICAL, length=255, mode="determinate", maximum=255, variable=self.left_g)
-        self.progress_channel1_b = ttk.Progressbar(self, orient=tk.VERTICAL, length=255, mode="determinate", maximum=255, variable=self.left_b)
-        #self.progress_channel2 = ttk.Progressbar(self, orient=tk.VERTICAL, length=255, mode="determinate", maximum=255)
-        #self.progress_channel3 = ttk.Progressbar(self, orient=tk.VERTICAL, length=255, mode="determinate", maximum=255)
-        #self.progress_channel4 = ttk.Progressbar(self, orient=tk.VERTICAL, length=255, mode="determinate", maximum=255, variable=self.i0)
-
-        #self.progress_channel1_r["background"] = "red"
-        self.content.grid(column=5, row=1)
-        #self.progress_channel0.grid(column=0, row=0)
-        self.progress_channel1_r.grid(column=0, row=0)
-        self.progress_channel1_g.grid(column=1, row=0)
-        self.progress_channel1_b.grid(column=2, row=0)
-        #self.progress_channel2.grid(column=2, row=0)
-        #self.progress_channel3.grid(column=3, row=0)
-        #self.progress_channel4.grid(column=4, row=0)
+        self.channel_1 = ChannelWidget(self.mainframe)
+        self.channel_2 = ChannelWidget(self.mainframe)
+        self.channel_3 = ChannelWidget(self.mainframe)
+        self.channel_4 = ChannelWidget(self.mainframe)
+        
+        self.channel_1.grid(column=0, row=0)
+        self.channel_2.grid(column=1, row=0)
+        self.channel_3.grid(column=2, row=0)
+        self.channel_4.grid(column=3, row=0)
 
         #self.button = ttk.Button(text="start", command=self.start)
         #self.button.grid(column=3, row=0)
@@ -76,7 +95,7 @@ class AtmoDummy(tk.Tk):
         #self.button5 = ttk.Button(text="start5", command=self.start5)
         #self.button5.grid(column=4, row=0)
 
-        self.after(100, self.update_gui)
+        self.after(50, self.update_gui)
 
     #def start(self):
         #self.progress_channel0["value"] = 0
@@ -97,10 +116,11 @@ class AtmoDummy(tk.Tk):
             #self.after(75, self.read_bytes5)
 
     def update_gui(self):
-        self.left_r.set(channel_left[0])
-        self.left_g.set(channel_left[1])
-        self.left_b.set(channel_left[2])
-        self.after(100, self.update_gui)
+        self.channel_1.update(protocol[ 7], protocol[ 8], protocol[ 9])
+        self.channel_2.update(protocol[10], protocol[11], protocol[12])
+        self.channel_3.update(protocol[13], protocol[14], protocol[15])
+        self.channel_4.update(protocol[16], protocol[17], protocol[18])
+        self.after(50, self.update_gui)
 
 if __name__ == "__main__":
     try:
